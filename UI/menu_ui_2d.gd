900f53c052e89f4bdfe700ef2b8416b0a56ae2f6
marcos-arthur <marcos.marcos.arthur291@gmail.com>
2026-02-01 09:43:10 -0300

    Basic physics example and minor fixes

--- UI/menu_ui_2d.gd
+++ UI/menu_ui_2d.gd
@@ -1,9 +1,18 @@
 extends Control
 
+func _input(event):
+	if event is InputEventMouseMotion:
+		# Move our cursor
+		var mouse_motion : InputEventMouseMotion = event
+		$Cursor.position = mouse_motion.position - Vector2(16, 16)
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://world.tscn")
+	get_tree().change_scene_to_file("res://level/world/world.tscn")
 
 
 func _on_button_2_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://level/world_hand_tracking/world.tscn")
